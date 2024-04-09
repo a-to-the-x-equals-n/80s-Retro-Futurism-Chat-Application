705e7756d5db59b28c76e12dead56d93eb73886d
Logan Reine <log.a.of.n@gmail.com>
2024-04-09 11:37:51 -0400

    fixed welcome screen text animation

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ def main():
     """
     x,y = util.terminal_size()
     
-    init_screen(x,y)
+    welcome_screen(x,y)
+
+
     
 
 
-def init_screen(x,y):
+def welcome_screen(x,y):
     """
     Initialize the screen with animated text effects.
 
@@ -27,9 +29,28 @@ def init_screen(x,y):
     tunnel_thread.join()
     dark_thread.join()
 
-    time.sleep(0.2)
-    sys.stdout.write(f"\033[{x//2+10};{y//2-6}H" + util.fade_text(f"-[ENTER]-", 0, 255, 100))
-        
+
+    dark_tunnel_display = []
+    dark_tunnel_display.extend(util.dark.split('\n'))
+    dark_tunnel_display.extend(util.tunnel.split('\n'))
+
+   
+    start = x//2-6
+    step = 0
+    while step < 255:
+        util.clear()
+        sys.stdout.flush()  # Flush the buffer to ensure immediate printing
+        for row, line in enumerate(dark_tunnel_display):
+            cursor = f"\033[{start+row};{39}H"
+            sys.stdout.write(cursor + util.fade_text(line, 215, 0, 255))
+        time.sleep(0.2)
+        step+=30
+        sys.stdout.write(f"\033[{x//2};{y//2-10}H" + util.fade_text(f"-[PRESS ENTER]-", red = 0, green = step, blue = step//2))
+
+    sys.stdout.write(f"\033[{x//2};{y//2-10}H" + util.fade_text(f"-[PRESS ENTER]-", red = 0, green = 255, blue = step//2))
+
+    
+    # Wait for user to press `Enter`
     input()
 
 
@@ -58,7 +79,6 @@ def tunnel(x, y):
         sys.stdout.flush()  # Flush the buffer to ensure immediate printing
         time.sleep(0.3)
         step+=15
-
     for row, line in enumerate(tunnel):
             cursor = f"\033[{start+row};{y-42-i}H"
             sys.stdout.write(cursor + util.fade_text(line, 215, 0, 255))
@@ -88,6 +108,8 @@ def dark(x, y):
     for row, line in enumerate(dark):
             cursor = f"\033[{start+row};{i}H"
             sys.stdout.write(cursor + util.fade_text(line, 215, 0, 255))
+
+
 
 if __name__ == "__main__": 
     main()
