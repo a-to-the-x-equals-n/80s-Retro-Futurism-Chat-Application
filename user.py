ef6e2e8ea5c13916e2c741a41d618e58c64a25e3
Logan Reine <log.a.of.n@gmail.com>
2024-04-17 02:16:30 -0400

    Optimized the functionality of the user object, and moved listening and searching to user

--- user.py
+++ user.py
@@ -1,34 +1,31 @@
 import socket as socket
-import util
+
+# from loading_interface import loading
+
 """
 User object for ip addr, port numbers, and username
 """
 class User:
-
+    
     def __init__(self, ip = 'localhost', port = 8080, username = 'default'):
         self.ip = ip
         self.port = port
         self.username = f'[{username}]:~$'
-        self.socket = None
-        self.connection = None
-        self.address = None
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
-    @util.threaded
-    def start_server(self, connection_event):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-            server_socket.bind((self.ip, self.port))
-            server_socket.listen()
-            
-            self.connection, self.address = server_socket.accept()
-            connection_event.set()  # Signal that a connection has been made
-
-    def find_server():
-        pass
         
-
     # Return a string representation of the User.username
-    def __repr__(self):
-        return f'{self.username}'
     def __str__(self):
         return f'{self.username}'
+    
+    def __len__(self):
+        return len(self.username)
+
+    def listening(self):
+        self.socket.bind((self.ip, self.port))
+        self.socket.listen(1)
+
+
+    def searching(self):
+        server_addr = (self.ip, self.port)
+        return server_addr
